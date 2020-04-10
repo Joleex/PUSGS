62432bd497beb2984810db02342dd957d2d0bbc9
Jovana Stevovic <jovanastevovic95@gmail.com>
2020-04-10 02:33:20 +0200

    Radio button za odabir tipa puta(round-trip, one-way, multi-city)

--- projekat/src/app/fly/fly/fly.component.ts
+++ projekat/src/app/fly/fly/fly.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { FlyCarComponent } from '../fly-car/fly-car.component';
+import { CarComponent } from '../car/car.component';
 
 @Component({
   selector: 'app-fly',
   templateUrl: './fly.component.html',
   styleUrls: ['./fly.component.css']
 })
-export class FlyComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit(): void {
+
+export class FlyComponent implements OnInit {
+  radioSelected: string;
+  selectedRadio: string='Round-trip';
+  radios: any =[
+    'Round-trip',
+    'One way',
+    'Multi-city'
+  ]
+  
+  radioChangeHandler(event:any){
+    this.selectedRadio=event.target.value;
+  }
+  constructor() {
   }
 
+  ngOnInit(): void {
+  
+  }
 }
